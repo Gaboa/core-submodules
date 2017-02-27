367d9b860f82e240cff1da844fb087d8ad556c05
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-27 17:49:38 +0200

    Add pause and resume methods to Sprite

--- markup/components/sprite/sprite.js
+++ markup/components/sprite/sprite.js
@@ -1,21 +1,37 @@
 export default class Sprite extends PIXI.Sprite {
     constructor({
         texture,
+        container,
         x = 0,
         y = 0,
-        container,
         anchor = 0.5
     }) {
         super(PIXI.utils.TextureCache[texture]);
+
         this.x = x;
         this.y = y;
         this.anchor.x = anchor;
         this.anchor.y = anchor;
-        container.addChild(this);
+
+        if (!texture) {
+            throw new Error('Texture parameter is undefined!');
+        }
+        if (container) {
+            container.addChild(this);
+        }
+
         this.handlers();
     }
     handlers() {}
-    update() {}
+    update() {
+        if (this.paused) return;
+    }
+    pause() {
+        this.paused = true;
+    }
+    resume() {
+        this.paused = false;
+    }
     destroy() {
         this.parent.removeChild(this);
     }
